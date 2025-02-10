21a327a08e306dc709fb781767c43b192d23e53d
SarunHirachan10 <90051714+SarunHirachan@users.noreply.github.com>
2025-02-10 22:09:41 +0545

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -34,6 +34,9 @@ export default function Header() {
           🐙 GitHub
         </a>
       </div>
+      <p className="text-text-primary max-w-2xl mx-auto">
+        I am a passionate and curious individual with a strong interest in technology, innovation, and entrepreneurship. With experience in programming, event organizing, and leadership, I excel at problem-solving and adapting to new challenges. Through hands-on projects and strategic roles, I have developed the skills to execute ideas effectively and create meaningful impact. I am always eager to learn, grow, and take on new opportunities.
+      </p>
     </motion.header>
   )
 }
