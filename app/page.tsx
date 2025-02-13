20cad0e924e70add8b0ab286557868c8c2e25d69
SarunHirachan10 <90051714+SarunHirachan@users.noreply.github.com>
2025-02-13 12:18:07 +0545

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
+import AnimatedBackground from "../components/AnimatedBackground"
 import Header from "../components/Header"
 import Skills from "../components/Skills"
 import Experience from "../components/Experience"
 import Volunteering from "../components/Volunteering"
 import Awards from "../components/Awards"
 import Education from "../components/Education"
-import Navigation from "../components/Navigation"
 
 export default function Home() {
   return (
     <main className="min-h-screen relative">
-      <Navigation />
-      <div className="container mx-auto px-4 py-8">
+      <AnimatedBackground />
+      <div className="container mx-auto px-4 py-8 relative z-10">
         <Header />
         <Skills />
         <Experience />
