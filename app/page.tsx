f9c41f6ad3bf854cab83b637a08773edf23351c2
SarunHirachan10 <90051714+SarunHirachan@users.noreply.github.com>
2025-04-16 23:11:00 +0545

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import AnimatedBackground from "../components/AnimatedBackground"
-import Header from "../components/Header"
-import Skills from "../components/Skills"
-import Experience from "../components/Experience"
-import Volunteering from "../components/Volunteering"
-import Awards from "../components/Awards"
-import Education from "../components/Education"
+import AnimatedBackground from "../components/AnimatedBackground";
+import Header from "../components/Header";
+import About from "../components/About"; // New import
+import Skills from "../components/Skills";
+import Experience from "../components/Experience";
+import Volunteering from "../components/Volunteering";
+import Awards from "../components/Awards";
+import Education from "../components/Education";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <AnimatedBackground />
       <div className="container mx-auto px-4 py-8 relative z-10">
         <Header />
+        <About /> {/* Added About section */}
         <Skills />
         <Experience />
         <Volunteering />
@@ -19,5 +21,5 @@ export default function Home() {
         <Education />
       </div>
     </main>
-  )
+  );
 }
