4650a105647fad469550a7e9dcd55e5939ae7ec3
SarunHirachan10 <90051714+SarunHirachan@users.noreply.github.com>
2025-04-20 23:38:19 +0545

    Update Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,4 +1,4 @@
-'use client'; // ADD THIS AT THE VERY TOP
+'use client' // Add this at very top
 
 import { motion, useScroll, useSpring } from "framer-motion"
 import { useEffect, useState } from "react"
