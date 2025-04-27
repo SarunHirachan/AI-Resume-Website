70cf42fb4d67daaed202b00fe115c76ceb38e3aa
SarunHirachan10 <90051714+SarunHirachan@users.noreply.github.com>
2025-04-27 23:23:04 +0545

    Update AnimatedBackground.tsx

--- components/AnimatedBackground.tsx
+++ components/AnimatedBackground.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useRef } from "react"
+import { createContext } from 'react';
 
 export default function AnimatedBackground() {
   const canvasRef = useRef<HTMLCanvasElement>(null)
